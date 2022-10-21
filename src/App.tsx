42cdc9ddc43e166d284e54d47f1c3477976e243f
Kurmanaliev Bekzhan <gethardlyever@gmail.com>
2022-10-21 15:07:01 +0600

    Include circle component to the main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Circle from "./Circle/Circle";
 
 function App() {
+
+  const mixLoop = () => {
+
+    let myArr: number[] = [];
+
+    for (let i = 0; i < 5; i++) {
+      let randomNumber: number = Math.floor(Math.random() * (36 - 5 + 1) + 5);
+      for (let j = 0; j < 5; j++) {
+        if (randomNumber === myArr[j]) {
+          randomNumber = Math.floor(Math.random() * (36 - 5 + 1) + 5);
+        }
+      }
+      myArr.push(randomNumber);
+    }
+    myArr.sort((a,b) => a -b);
+
+    return myArr
+  };
+
+  const [randNumb, setNumber] = useState(mixLoop());
+
+  const changeNumbers = () => {
+    setNumber(mixLoop());
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Circle randomNumber={randNumb[0]}>
+      </Circle>
+      <Circle randomNumber={randNumb[1]}>
+      </Circle>
+      <Circle randomNumber={randNumb[2]}>
+      </Circle>
+      <Circle randomNumber={randNumb[3]}>
+      </Circle>
+      <Circle randomNumber={randNumb[4]}>
+      </Circle>
+
+      <div>
+        <button onClick={changeNumbers}>New numbers</button>
+      </div>
     </div>
   );
 }
